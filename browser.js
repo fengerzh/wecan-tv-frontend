354c0f854e79f26f536aefbea0090f03938ef59e
ZhangBuzz <buzz.zhang@gmail.com>
2016-09-19 14:15:18 +0800

    add eslint config

--- browser.js
+++
@@ -1,5 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-const mainApp = React.createFactory(require('./src/main.jsx'));
-
-ReactDOM.render(mainApp(window.APP_PROPS), document.getElementById('content'));

93d152b3de0c2b85d7cd2d23144f703a672461e7
yangtoude <yefeng_yue@163.com>
2016-09-22 16:04:52 +0800

    cyberplayer add style

--- src/components/LiveView.jsx
+++ src/components/LiveView.jsx
@@ -1,15 +1,25 @@
 import React, { Component, PropTypes } from 'react';
 
 class LiveView extends Component {
-  componentDidMount() {
+  componentWillMount() {
     this.props.fetchLive(this.props.params.liveId);
+  }
 
+  componentDidMount() {
     const script = document.createElement('script');
+    // RTMP输出
     script.src = '/cyberplayer.js?file=rtmp://play.bcelive.com/live'
-      + '/lss-giftivhkx29rmrug&width=680&autostart=true&volume=60&height=400'
+      + `/${this.props.params.liveId}&width=680&autostart=true&volume=60&height=400`
       + '&ak=db1c5c6d9c4347e2936fdd007a5ee804&stretching=uniform&controls=true';
+    // FLV输出
+    // script.src = '/cyberplayer.js?file=http://play.bcelive.com/live'
+    //   + `/${this.props.params.liveId}.flv&width=680&autostart=true&volume=60&height=400`
+    //   + '&ak=db1c5c6d9c4347e2936fdd007a5ee804&stretching=uniform&controls=true';
+    // HLS输出
+    // script.src = '/cyberplayer.js?file=http://gh0sbbe1c1x0h58nfad.exp.bcelive.com'
+    //   + `/${this.props.params.liveId}live.m3u8&width=680&autostart=true&volume=60&height=400`
+    //   + '&ak=db1c5c6d9c4347e2936fdd007a5ee804&stretching=uniform&controls=true';
     script.async = true;
-
     document.getElementById('player').appendChild(script);
   }
 
